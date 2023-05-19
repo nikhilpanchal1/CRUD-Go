cea15bbebff0a0086e97c8098fb19c26a4d82125
Nikhil Panchal <37629345+nikhilpanchal1@users.noreply.github.com>
2023-05-19 15:07:42 -0600

    added model.go that satisfies all requirements

---
+++ database/model.go
@@ -0,0 +1,41 @@
+package database
+
+import (
+	"github.com/google/uuid"
+	"golang.org/x/crypto/bcrypt"
+)
+
+type Item struct {
+	ID    string  `json:"id"`
+	Name  string  `json:"name"`
+	Price float64 `json:"price"`
+}
+
+type User struct {
+	ID       string `json:"id"`
+	Username string `json:"username"`
+	Password string `json:"-"`
+}
+
+// New item with auto gen id
+func NewItem(name string, price float64) *Item {
+	return &Item{
+		ID:    uuid.New().String(), //there is an alternative os/exec that can be used for the same. https://www.geeksforgeeks.org/generate-uuid-in-golang/#
+		Name:  name,
+		Price: price,
+	}
+}
+
+// User with auto gen id and hashed passwd
+func NewUser(username, password string) (*User, error) {
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	if err != nil {
+		return nil, err
+	}
+
+	return &User{
+		ID:       uuid.New().String(),
+		Username: username,
+		Password: string(hashedPassword),
+	}, nil
+}
