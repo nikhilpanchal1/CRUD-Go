59910d90e8f2a0035483d13e6465b26c8a52503a
Nikhil Panchal <37629345+nikhilpanchal1@users.noreply.github.com>
2023-05-19 17:55:34 -0600

    Docker init
    Back to baby steps

---
+++ Dockerfile
@@ -0,0 +1,4 @@
+#FROM golang:1.19.0
+FROM golang:1.20.4-bullseye
+
+WORKDIR /usr/src/app
--- main.go
+++ main.go
@@ -1,13 +1,21 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+	//import model.go
+)
 
 func main() {
 	app := fiber.New()
 
-	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("Welcome to Go Fiber API")
-	})
+	app.Use(logger.New())
+
+	app.Get("/items", getItems)
+	app.Post("/items", createItem)
+	app.Delete("/items/:id", deleteItem)
+	app.Post("/login", loginUser)
+	app.Get("/user", getUser)
 
 	app.Listen(":5000")
 }
