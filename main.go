f0c00a6ebca20a0a5f56a2c7f20acecafc493429
Nikhil Panchal <37629345+nikhilpanchal1@users.noreply.github.com>
2023-05-19 14:35:03 -0600

    init

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import "github.com/gofiber/fiber/v2"
+
+func main() {
+	app := fiber.New()
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Welcome to Go Fiber API")
+	})
+
+	app.Listen(":5000")
+}
