3f84d396b98816ac1cb35ca2022c6d745bda02af
Nikhil Panchal <37629345+nikhilpanchal1@users.noreply.github.com>
2023-05-19 18:27:36 -0600

    In theory, should support hot-reload, BUT JUST DOESNT
    We are indeed very very lucky :alien:

--- Dockerfile
+++ Dockerfile
@@ -2,3 +2,9 @@
 FROM golang:1.20.4-bullseye
 
 WORKDIR /usr/src/app
+
+#hot reload
+RUN go install github.com/cosmtrek/air@latest
+
+COPY . .
+RUN go mod tidy
--- cmd/main.go
+++ cmd/main.go
@@ -2,6 +2,20 @@ package main
 
 import (
 	"github.com/gofiber/fiber/v2"
+)
+
+func main() {
+	app := fiber.New()
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString(" Hello, Not!")
+	})
+	app.Listen(":3000")
+}
+
+/*
+import (
+	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/logger"
 	//import model.go
 )
@@ -17,5 +31,6 @@ func main() {
 	app.Post("/login", loginUser)
 	app.Get("/user", getUser)
 
-	app.Listen(":5000")
+	app.Listen(":3000")
 }
+*/
